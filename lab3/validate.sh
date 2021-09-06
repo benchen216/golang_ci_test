8959f91cd9ea37feec67c4543a509ef1e6db234a
ben <benchen216@gmail.com>
2021-09-06 10:56:48 +0800

    fix lab3

--- lab3/validate.sh
+++ lab3/validate.sh
@@ -2,29 +2,20 @@
 
 test_path="${BASH_SOURCE[0]}"
 solution_path="$(realpath .)"
-tmp_dir=$(mktemp -d -t lab1-XXXXXXXXXX)
+tmp_dir=$(mktemp -d -t lab3-XXXXXXXXXX)
 
 echo "working directory: $tmp_dir"
 cd $tmp_dir
 
-#rm -rf *
-cp $solution_path/lab1.go .
-action=1
-a=$((1 + RANDOM % 10))
-b=$((1 + RANDOM % 10))
-ans=$((a+b))
-result=$(printf '%d\n%d %d' $action $a $b | go run lab1.go 2>&1 | tail -1 ) ; ret=$?
-if [ $ret -ne 0 ] ; then
-  echo "\"go run lab1.go\" fails ; NO POINT"
+rm -rf *
+cp $solution_path/lab3.go .
+go get github.com/adonovan/gopl.io/ch4/github
+go run lab3.go&
+if [ "$(curl http://localhost:8080/ )" != "$(curl http://nctu.is-geek.com:8083/ )" ] ; then
+  echo "wrong answer ; NO POINT"
 else
-  echo "\"go run lab1.go\" output: \"$result\""
-  if [ "$(echo $ans)" != "$(echo $result)" ] ; then
-    echo "wrong answer ; NO POINT"
-  else
-    echo "GET POINT 1"
-  fi
+  echo "GET POINT 1"
 fi
-
 
 echo "deleting working directory $tmp_dir"
 rm -rf $tmp_dir
