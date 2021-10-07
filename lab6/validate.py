3c9afbf3f5b8a504d60de0b2725964ccf521593f
ben <benchen216@gmail.com>
2021-10-08 00:33:30 +0800

    test lab6

---
+++ lab6/validate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import time
+import os
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException, WebDriverException
+
+time.sleep(5)
+options = Options()
+options.headless = True
+options.add_argument('--no-sandbox') # https://stackoverflow.com/a/45846909
+options.add_argument('--disable-dev-shm-usage') # https://stackoverflow.com/a/50642913
+chrome = Chrome(options=options)
+
+chrome.get(f"http://0.0.0.0/index.html")
