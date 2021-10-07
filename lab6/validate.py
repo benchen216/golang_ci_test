000ceaf9ea34ba00e5d2e321bf5f7f150bfc55d3
ben <benchen216@gmail.com>
2021-10-08 00:53:43 +0800

    test lab6

--- lab6/validate.py
+++ lab6/validate.py
@@ -11,6 +11,7 @@ options = Options()
 options.headless = True
 options.add_argument('--no-sandbox') # https://stackoverflow.com/a/45846909
 options.add_argument('--disable-dev-shm-usage') # https://stackoverflow.com/a/50642913
-chrome = Chrome(ChromeDriverManager(version="83.0.4103.39").install(),options=options)
+chrome = Chrome("/usr/lib/chromium-browser/chromedriver",options=options)
 
+#ChromeDriverManager(version="83.0.4103.39").install()
 chrome.get(f"http://0.0.0.0/index.html")
