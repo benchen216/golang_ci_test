96a1d51f21b6a22a049a2c76462e0b2d0d80a017
ben <benchen216@gmail.com>
2021-10-08 00:40:37 +0800

    test lab6

--- lab6/validate.py
+++ lab6/validate.py
@@ -5,12 +5,12 @@ import os
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException, WebDriverException
-
+from webdriver_manager.chrome import ChromeDriverManager
 time.sleep(5)
 options = Options()
 options.headless = True
 options.add_argument('--no-sandbox') # https://stackoverflow.com/a/45846909
 options.add_argument('--disable-dev-shm-usage') # https://stackoverflow.com/a/50642913
-chrome = Chrome(options=options)
+chrome = Chrome(options=options,ChromeDriverManager(version="83.0.4103.116").install())
 
 chrome.get(f"http://0.0.0.0/index.html")
